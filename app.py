f40ac242d019dfd4bb57ca13d392362d1ef7db6c
waffers <john.foong32@gmail.com>
2023-09-09 19:00:40 +0800

    template folder added

--- app.py
+++ app.py
@@ -21,5 +21,9 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("layout.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
     
